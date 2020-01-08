3427a8efb203e1ffa95985bf5ecb0c0cf8b464b5
timkrentz <tskrentz@gmail.com>
2020-01-08 11:29:20 -0600

    Updated, more similar to thinkpad

--- keyboards/ergodox_ez/keymaps/tim/keymap.c
+++ keyboards/ergodox_ez/keymaps/tim/keymap.c
@@ -18,15 +18,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |  =+  |           |  `~  |   6  |   7  |   8  |   9  |   0  |   |    |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |  =+  |           |  `~  |   6  |   7  |   8  |   9  |   0  |   -_   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | =+     |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   -_   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   |\   |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ; :  | ' "    |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ; :  | ' "    |
  * |--------+------+------+------+------+------|  (   |           |  )   |------+------+------+------+------+--------|
- * | LCtrl  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | / ?  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | / ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LAlt  | LWin | >/|| | Home | End  |                                       | Left | Down |  Up  | Right| ~L1  |
+ *   |LAlt  | LWin | >/|| | Home | End  |                                       | Left | Down |  Up  | Right| RAlt  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | SYMB | LGui |       | RAlt |RCtrl   |
@@ -41,19 +41,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
-        KC_EQL,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRACKET,
-        KC_TAB,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LCTRL,       KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_LPRN,
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRACKET,
+        KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,       KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_LPRN,
         KC_LALT,KC_LGUI, KC_MPLY ,  KC_HOME,KC_END,
                                               MO(SYMB),  KC_LGUI,
                                                               KC_HOME,
                                                KC_LSFT,KC_BSPC,KC_DELT,
         // right hand
-             KC_GRAVE,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSLASH,
-             KC_RBRACKET,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
+             KC_GRAVE,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_RBRACKET,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLASH,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_RPRN,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-                                  KC_LEFT, KC_DOWN,KC_UP,KC_RIGHT,          KC_FN1,
+                                  KC_LEFT, KC_DOWN,KC_UP,KC_RIGHT,          KC_RALT,
              KC_RALT,        KC_RCTRL,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
